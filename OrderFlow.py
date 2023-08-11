7ef32f6f9b9e81c2bdfccd8bf6a91adc49a1994b
Murtaza Ali <psuedopunk007@gmail.com>
2023-08-11 22:52:24 +0530

    Add your own imbalance ratios

---
+++ OrderFlow.py
@@ -0,0 +1,324 @@
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import plotly.io as pio
+from plotly.graph_objs import *
+from plotly.subplots import make_subplots
+import warnings
+import time
+import string
+import random
+
+warnings.filterwarnings('ignore')
+
+class OrderFlowChart():
+    def __init__(self, orderflow_data, ohlc_data, identifier_col=None, imbalance_col=None):
+        """
+        The constructor for OrderFlowChart class.
+        It takes in the orderflow data and the ohlc data and creates a unique identifier for each candle if not provided.
+        It also calculates the imbalance if not provided.
+        The data should have datetime index and should have the following columns:
+        orderflow_data: ['bid_size', 'price', 'ask_size', 'identifier']
+        ohlc_data: ['open', 'high', 'low', 'close', 'identifier']
+
+        The identifier column is used to map the orderflow data to the ohlc data.
+        """
+        self.orderflow_data = orderflow_data
+        self.ohlc_data = ohlc_data
+        self.identifier_col = identifier_col
+        self.imbalance_col = imbalance_col
+
+    def generate_random_string(self, length):
+        letters = string.ascii_letters
+        return ''.join(random.choice(letters) for _ in range(length))
+
+    def create_identifier(self):
+        """This method will generate a unique gibberish string for each candle based on the timestamp and the price.
+        """
+        identifier = [self.generate_random_string(5) for i in range(self.ohlc_data.shape[0])]
+        self.ohlc_data['identifier'] = identifier
+        self.orderflow_data['identifier'] = self.ohlc_data['identifier']
+
+    def calc_imbalance(self, df):
+        df['sum'] = df['bid_size'] + df['ask_size']
+        df['time'] = df.index.astype(str)
+        bids, asks = [], []
+        for b, a in zip(df['bid_size'].astype(int).astype(str),
+                        df['ask_size'].astype(int).astype(str)):
+            dif = 4 - len(a)
+            a = a + (' ' * dif)
+            dif = 4 - len(b)
+            b = (' ' * dif) + b
+            bids.append(b)
+            asks.append(a)
+
+        df['text'] = pd.Series(bids, index=df.index) + '  ' + \
+            pd.Series(asks, index=df.index)
+        df.index = df['identifier']
+        
+        if self.imbalance_col is None:
+            print("Calculating imbalance, as no imbalance column was provided.")
+            df['size'] = (df['bid_size'] - df['ask_size']) / \
+                (df['bid_size'] + df['ask_size'])
+        else:
+            print("Using imbalance column: {}".format(self.imbalance_col))
+            df['size'] = df[self.imbalance_col]
+            df = df.drop([self.imbalance_col], axis=1)
+        # df = df.drop(['bid_size', 'ask_size'], axis=1)
+        return df
+
+    def annotate(self, df2):
+        df2 = df2.drop(['size'], axis=1)
+        df2['sum'] = df2['sum'] / df2.groupby(df2.index)['sum'].transform('max')
+        df2['text'] = ''
+        df2['time'] = df2['time'].astype(str)
+        df2['text'] = ['█' * int(sum_ * 10) for sum_ in df2['sum']]
+        df2['text'] = '                    ' + df2['text']
+        df2['time'] = df2['time'].astype(str)
+        return df2
+
+    def range_proc(self, ohlc, type_='hl'):
+        if type_ == 'hl':
+            seq = ohlc['low'].append(ohlc['high'])
+        if type_ == 'oc':
+            seq = ohlc['open'].append(ohlc['close'])
+        id_seq = ohlc['identifier'].append(ohlc['identifier'])
+        seq_hl = ohlc['sequence'].append(ohlc['sequence'])
+        seq = pd.DataFrame(seq, columns=['price'])
+        seq['identifier'] = id_seq
+        seq['sequence'] = seq_hl
+        seq['time'] = seq.index
+        seq = seq.sort_index()
+        seq = seq.set_index('identifier')
+        return seq
+
+    def candle_proc(self, df):
+        df = df.sort_values(by=['time', 'sequence', 'price'])
+        df = df.reset_index()
+        df_dp = df.iloc[1::2].copy()
+        df = pd.concat([df, df_dp])
+        df = df.sort_index()
+        df = df.set_index('identifier')
+        df = df.sort_values(by=['time', 'sequence'])
+        # df = df.sort_index()
+        df[2::3] = np.nan
+        return df
+
+    def calc_params(self, of, ohlc):
+        delta = of.groupby(of['identifier']).sum()['ask_size'] - \
+            of.groupby(of['identifier']).sum()['bid_size']
+        delta = delta[ohlc['identifier']]
+        cum_delta = delta.rolling(10).sum()
+        roc = cum_delta.diff()/cum_delta.shift(1) * 100
+        roc = roc.fillna(0).round(2)
+        volume = of.groupby(of['identifier']).sum()['ask_size'] + of.groupby(of['identifier']).sum()['bid_size']
+        delta = pd.DataFrame(delta, columns=['value'])
+        delta['type'] = 'delta'
+        cum_delta = pd.DataFrame(cum_delta, columns=['value'])
+        cum_delta['type'] = 'cum_delta'
+        roc = pd.DataFrame(roc, columns=['value'])
+        roc['type'] = 'roc'
+        volume = pd.DataFrame(volume, columns=['value'])
+        volume['type'] = 'volume'
+
+        labels = pd.concat([delta, cum_delta, roc, volume])
+        labels = labels.sort_index()
+        labels['text'] = labels['value'].astype(str)
+
+        labels['value'] = np.tanh(labels['value'])
+        # raise Exception
+        return labels
+
+    def plot_ranges(self, ohlc):
+        ymin = ohlc['high'][-1] + 1
+        ymax = ymin - 12
+        xmax = ohlc.shape[0]
+        xmin = xmax - 9
+        tickvals = [i for i in ohlc['identifier']]
+        ticktext = [i for i in ohlc.index]
+        return ymin, ymax, xmin, xmax, tickvals, ticktext
+
+    def plot(self):
+        if self.identifier_col is None:
+            self.create_identifier()
+    
+        df = self.calc_imbalance(self.orderflow_data)
+
+        df2 = self.annotate(df.copy())
+
+        green_id = self.ohlc_data.loc[self.ohlc_data['close'] >= self.ohlc_data['open']]['identifier']
+        red_id = self.ohlc_data.loc[self.ohlc_data['close'] < self.ohlc_data['open']]['identifier']
+
+        high_low = self.range_proc(self.ohlc_data, type_='hl')
+        green_hl = high_low.loc[green_id]
+        green_hl = self.candle_proc(green_hl)
+
+        red_hl = high_low.loc[red_id]
+        red_hl = self.candle_proc(red_hl)
+
+        open_close = self.range_proc(self.ohlc_data, type_='oc')
+
+        green_oc = open_close.loc[green_id]
+        green_oc = self.candle_proc(green_oc)
+
+        red_oc = open_close.loc[red_id]
+        red_oc = self.candle_proc(red_oc)
+
+        labels = self.calc_params(self.orderflow_data, self.ohlc_data)
+
+        ymin, ymax, xmin, xmax, tickvals, ticktext = self.plot_ranges(self.ohlc_data)
+        print("Total candles: ", self.ohlc_data.shape[0])
+        # Create figure
+        fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
+                            vertical_spacing=0.0, row_heights=[9, 1])
+
+        fig.add_trace(go.Scatter(x=df2['identifier'], y=df2['price'], text=df2['text'],
+                                name='VolumeProfile', textposition='middle right',
+                                textfont=dict(size=8, color='rgb(0, 0, 255, 0.0)'), hoverinfo='none',
+                                mode='text', showlegend=True,
+                                marker=dict(
+                                sizemode='area',
+                                sizeref=0.1,  # Adjust the size scaling factor as needed
+                                )), row=1, col=1)
+
+        # Add trace for orderflow data
+        fig.add_trace(
+            go.Heatmap(
+                x=df['identifier'],
+                y=df['price'],
+                z=df['size'],
+                text=df['text'],
+                colorscale='icefire_r',
+                showscale=False,
+                showlegend=True,
+                name='BidAsk',
+                texttemplate="%{text}",
+                textfont={
+                    "size": 11,
+                    "family": "Courier New"},
+                hovertemplate="Price: %{y}<br>Size: %{text}<br>Imbalance: %{z}<extra></extra>",
+                xgap=60),
+            row=1,
+            col=1)
+
+        fig.add_trace(
+            go.Scatter(
+                x=green_hl.index,
+                y=green_hl['price'],
+                name='Candle',
+                legendgroup='group',
+                showlegend=True,
+                line=dict(
+                    color='green',
+                    width=1.5)),
+            row=1,
+            col=1)
+
+        fig.add_trace(
+            go.Scatter(
+                x=red_hl.index,
+                y=red_hl['price'],
+                name='Candle',
+                legendgroup='group',
+                showlegend=False,
+                line=dict(
+                    color='red',
+                    width=1.5)),
+            row=1,
+            col=1)
+
+        fig.add_trace(
+            go.Scatter(
+                x=green_oc.index,
+                y=green_oc['price'],
+                name='Candle',
+                legendgroup='group',
+                showlegend=False,
+                line=dict(
+                    color='green',
+                    width=6)),
+            row=1,
+            col=1)
+
+        fig.add_trace(
+            go.Scatter(
+                x=red_oc.index,
+                y=red_oc['price'],
+                name='Candle',
+                legendgroup='group',
+                showlegend=False,
+                line=dict(
+                    color='red',
+                    width=6)),
+            row=1,
+            col=1)
+
+        # fig.add_trace(go.Scatter(x=buy_vwap.index, y=buy_vwap, name='VWAP', legendgroup='group', showlegend=True, line=dict(color='green', width=1.5)), row=1, col=1)
+
+        # fig.add_trace(go.Scatter(x=sell_vwap.index, y=sell_vwap, name='VWAP', legendgroup='group', showlegend=False, line=dict(color='red', width=1.5)), row=1, col=1)
+        
+        fig.add_trace(
+            go.Heatmap(
+                x=labels.index,
+                y=labels['type'],
+                z=labels['value'],
+                colorscale='rdylgn',
+                showscale=False,
+                showlegend=True,
+                name='Parameters',
+                text=labels['text'],
+                texttemplate="%{text}",
+                textfont={
+                    "size": 10},
+                hovertemplate="%{x}<br>%{text}<extra></extra>",
+                xgap=4,
+                ygap=4),
+            row=2,
+            col=1)
+
+        fig.update_layout(title='Order Book Chart',
+                        yaxis=dict(title='Price', showgrid=False, range=[
+                                    ymax, ymin], tickformat='.2f'),
+                        yaxis2=dict(fixedrange=True, showgrid=False),
+                        xaxis2=dict(title='Time', showgrid=False),
+                        xaxis=dict(showgrid=False, range=[xmin, xmax]),
+                        height=780,
+                        template='plotly_dark',
+                        paper_bgcolor='#222', plot_bgcolor='#222',
+                        dragmode='pan', margin=dict(l=10, r=0, t=40, b=20),)
+
+        fig.update_xaxes(
+            showspikes=True,
+            spikecolor="white",
+            spikesnap="cursor",
+            spikemode="across",
+            spikethickness=0.25,
+            tickmode='array',
+            tickvals=tickvals,
+            ticktext=ticktext)
+        fig.update_yaxes(
+            showspikes=True,
+            spikecolor="white",
+            spikesnap="cursor",
+            spikemode="across",
+            spikethickness=0.25)
+        fig.update_layout(spikedistance=1000, hoverdistance=100)
+
+        config = {
+            'modeBarButtonsToRemove': ['zoomIn', 'zoomOut', 'zoom', 'autoScale'],
+            'scrollZoom': True,
+            'displaylogo': False,
+            'modeBarButtonsToAdd': ['drawline',
+                                    'drawopenpath',
+                                    'drawclosedpath',
+                                    'drawcircle',
+                                    'drawrect',
+                                    'eraseshape'
+                                    ]
+        }
+        # Show figure
+        fig.show(config=config)
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# OrderflowChart
-This repository plots orderflow footprint charts using plotly in python.
+<h1>OrderflowChart</h1><p>Welcome to the <b>OrderflowChart</b> repository! This project empowers you to visualize orderflow footprint charts effortlessly using Python and Plotly.</p><h2>Usage</h2><p>The heart of the project is the <code>OrderFlowChart</code> class constructor. It's designed to simplify the process of plotting orderflow data on footprint charts, leveraging Plotly's capabilities.</p><h3>Constructor</h3><pre><div class="bg-black rounded-md mb-4"><div class="flex items-center relative text-gray-200 bg-gray-800 px-4 py-2 text-xs font-sans justify-between rounded-t-md"></div><div class="p-4 overflow-y-auto"><code class="!whitespace-pre hljs language-python"><span class="hljs-keyword">from</span> OrderFlow <span class="hljs-keyword">import</span> OrderFlowChart
 
-# Example
-![alt text](image.png)
+orderflowchart = OrderFlowChart(orderflow_data, ohlc_data, identifier_col=<span class="hljs-string">'identifier'</span>)
+orderflowchart.plot()
+</code></div></div></pre>
+<h3>Parameters</h3>
+<ul>
+<li><p>
+<code>orderflow_data</code>: Your orderflow data, containing columns like 'bid_size', 'price', 'ask_size', and 'identifier'. If the 'imbalance' column needs to be calculated, simply provide it along with the previous mentioned columns</p></li>
+<li><p>
+<code>ohlc_data</code>: Your OHLC data with columns 'open', 'high', 'low', 'close', and 'identifier'. The 'identifier' column bridges the gap between orderflow and OHLC data.</p></li>
+<li><p>
+<code>identifier_col</code>: The column that uniquely identifies candles in both datasets.</p></li>
+<li><p>
+<code>imbalance_col</code>: The column name that contains imbalance for each price level. Provide None if to be calculated.</p></li>
+</ul><h3>Output</h3><p>The above code snippet generates a stunning orderflow chart like this:</p><p><img src="image.png" alt="OrderFlowChart Example"></p><p>With OrderflowChart, you can effortlessly transform complex orderflow data into visually appealing and insightful footprint charts. Feel free to explore, customize, and gain new perspectives from your data with this powerful tool.</p>
